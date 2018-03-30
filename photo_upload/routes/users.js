6770039ae05b6ecee9c4e53a6feadfe9c86ae51b
薛伟 <xuew@zjubtv.com>
2018-03-30 20:07:48 +0800

    bb

--- photo_upload/routes/users.js
+++ photo_upload/routes/users.js
@@ -22,46 +22,46 @@ router.post('/upload', function(req, res, next) {
     form.encoding = 'utf-8'; //设置编辑
     form.uploadDir = 'public' + UPLOAD_FOLDER; //设置上传目录
     form.keepExtensions = true; //保留后缀
-    form.parse(req, function(err, fields, files) {
-        if (err) {
-            res.locals.error = err;
-            return;
-        }
-        var extName = ''; //后缀名
-        switch (files.photo.type) {
-            case 'image/pjpeg':
-                extName = 'jpg';
-                break;
-            case 'image/jpeg':
-                extName = 'jpg';
-                break;
-            case 'image/png':
-                extName = 'png';
-                break;
-            case 'image/x-png':
-                extName = 'png';
-                break;
-        }
-        console.log(fields);
-        console.log(files);
-        if (extName.length == 0) {
-            res.locals.error = '只支持png和jpg格式图片';
-            console.log("不好意思，上传只支持png和jpg格式照片")
-                //res.send("不好意思，上传只支持png和jpg格式照片");
-            return;
-        }
-
-        formData = fields;
-        photoPath = files.photo.path;
-        console.log(fields);
-        console.log(files.photo.path);
-    });
 
     form.on('progress', function(bytesReceived, bytesExpected) {
         console.log("当前进度" + bytesReceived + ";" + bytesExpected);
     });
 
     form.on('end', function() {
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+                res.locals.error = err;
+                return;
+            }
+            var extName = ''; //后缀名
+            switch (files.photo.type) {
+                case 'image/pjpeg':
+                    extName = 'jpg';
+                    break;
+                case 'image/jpeg':
+                    extName = 'jpg';
+                    break;
+                case 'image/png':
+                    extName = 'png';
+                    break;
+                case 'image/x-png':
+                    extName = 'png';
+                    break;
+            }
+            console.log(fields);
+            console.log(files);
+            if (extName.length == 0) {
+                res.locals.error = '只支持png和jpg格式图片';
+                console.log("不好意思，上传只支持png和jpg格式照片")
+                    //res.send("不好意思，上传只支持png和jpg格式照片");
+                return;
+            }
+
+            formData = fields;
+            photoPath = files.photo.path;
+            console.log(fields);
+            console.log(files.photo.path);
+        });
         uploadModel.uploadPhoto(req, res, formData, photoPath);
         //res.send("上传成功！");
     });
